923b23795f95ca0ab52d27094d5876e81e3cdc8b
Meng <leo80625@gmail.com>
2017-06-10 20:27:53 +0800

    新增登入系統

--- js/all.js
+++ js/all.js
@@ -49,3 +49,36 @@ $(document).ready(function() {
 	});
 	$('.meals li:eq(3) .add').click();
 });
+
+var body=document.body;
+var xhr=new XMLHttpRequest();
+var mail=document.getElementById('login_mail');
+var password=document.getElementById('login_password');
+var login=document.querySelector('.buttom_login');
+var register=document.querySelector('.buttom_gotoregister');
+
+function sendData(e){
+	e.preventDefault();
+	xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signin',true);
+	xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
+	var str ='email='+mail.value+'&password='+password.value;
+	xhr.send(str);
+	password.value='';
+};
+
+login.addEventListener('click',sendData,false);
+
+register.addEventListener('click',function(e){
+	e.preventDefault();
+	location.href='https://wumenglin.github.io/Meng0605/';
+},false);
+
+xhr.onload=function(){
+	var array=[];
+	array=JSON.parse(xhr.responseText);
+	alert(array.message);
+}
+
+body.addEventListener('keydown',function(e){
+	if(e.keyCode=='13'){sendData(e);}
+},false);
